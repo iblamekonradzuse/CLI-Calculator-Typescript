4bad1099081778cee0c5cbe446074b1e347c2b40
cancaliskann <podcastwerv2017@gmail.com>
2024-07-23 15:59:17 +0200

    checking if types are matching

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hello wolrd");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var readline_sync_1 = require("readline-sync");
+function main() {
+    var firstStr = (0, readline_sync_1.question)('enter first number:\n');
+    var operator = (0, readline_sync_1.question)('enter operator:\n');
+    var secondStr = (0, readline_sync_1.question)('enter second number:\n');
+    var validInput = isNumber(firstStr) && isOperator(operator) && isNumber(secondStr);
+    console.log(validInput);
+}
+function isOperator(operator) {
+    switch (operator) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            return true;
+        default:
+            return false;
+    }
+}
+function isNumber(str) {
+    var maybeNum = parseInt(str);
+    var isNum = !isNaN(maybeNum);
+    return isNum;
+}
+main();
--- index.ts
+++ index.ts
@@ -1,1 +1,35 @@
-console.log("hello world");
+import { question } from 'readline-sync';
+
+function main(): void {
+    const firstStr: string = question('enter first number:\n');
+    const operator: string = question('enter operator:\n');
+    const secondStr: string = question('enter second number:\n');
+
+    const validInput: boolean = isNumber(firstStr) && isOperator(operator) && isNumber(secondStr);
+
+    console.log(validInput);
+    
+}
+
+
+function isOperator(operator: string): boolean {
+    switch(operator){
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            return true;
+        default:
+            return false;
+    }
+}
+
+function isNumber(str: string ): boolean{
+
+    const maybeNum = parseInt(str);
+    const isNum: boolean = !isNaN(maybeNum);
+    return isNum;
+
+}
+
+main();
